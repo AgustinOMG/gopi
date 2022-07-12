4831835b702528fdf03190efd4a72c6fe0c7c028
Agustin Ortiz de Montellano Galeazzi <agustin.montellano@grupoeho.com>
2022-07-12 16:46:02 -0500

    mongodb connection

--- main.go
+++ main.go
@@ -1,8 +1,17 @@
 package main
 
 import (
+	"context"
+	"fmt"
+
 	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/mongo/readpref"
 )
+
+const uri = "mongodb://192.168.1.116:27017"
 
 type todo struct {
 	Id        string `json:"id"`
@@ -17,6 +26,34 @@ var todos = []todo{
 }
 
 func main() {
+	// Create a new client and connect to the server
+	client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI(uri))
+	if err != nil {
+		panic(err)
+	}
+	defer func() {
+		if err = client.Disconnect(context.TODO()); err != nil {
+			panic(err)
+		}
+	}()
+	// Ping the primary
+	if err := client.Ping(context.TODO(), readpref.Primary()); err != nil {
+		panic(err)
+	}
+
+	usersCollection := client.Database("testing").Collection("users")
+	// insert a single document into a collection
+	// create a bson.D object
+	user := bson.D{{"fullName", "User 1"}, {"age", 30}}
+	// insert the bson object using InsertOne()
+	result, err := usersCollection.InsertOne(context.TODO(), user)
+	// check for errors in the insertion
+	if err != nil {
+		panic(err)
+	}
+	// display the id of the newly inserted object
+	fmt.Println(result.InsertedID)
+
 	router := gin.Default()
 	apiRoutes(router)
 	router.Run("localhost:9090")
