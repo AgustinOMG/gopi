27075720b7d6e12ddc53ba78bb0c141f6cc90285
Agustin Ortiz de Montellano Galeazzi <agustin.montellano@grupoeho.com>
2022-07-25 14:43:57 -0500

    creo esta listo para muestra

--- functions.go
+++ functions.go
@@ -2,7 +2,8 @@ package main
 
 import (
 	"context"
-	"errors"
+
+	"fmt"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
@@ -11,64 +12,64 @@ import (
 )
 
 func addPart(data *gin.Context) {
+	var stationList = [5]string{"110", "120", "130", "140", "150"}
 
+	//declara variable para alamcenar datos en formato Map
 	stationData := make(map[string]string)
+	//Obtener el valor del ID de acuerdo al parametro de la URL
 	id := data.Param("id")
+	//Obtener ek parametro de la estacion
 	station := data.Param("station")
+	//Obtener el parametro de la linea de produccion
 	line := data.Param("line")
+	//Obtener los datos del ormato json y pasarlos a la variable de tipo mapa, si existe un error enviar mensaje de regreso
 	if err := data.BindJSON(&stationData); err != nil {
 		data.IndentedJSON(http.StatusNotImplemented, gin.H{"mensaje": "Formato de datos incorrecto"})
 	}
+	// Declaracion de la coleccion y base de datos a donde se quiere escribir
 	dbStation := Client.Database("kayser").Collection(line)
-	dbStation.InsertOne(context.TODO(), bson.D{{Key: "sn", Value: id}})
+
+	//Busca si existe ya una valor con ese Id.
+	errid := dbStation.FindOne(context.TODO(), bson.M{"sn": id}).Err()
+
+	if errid != nil {
+		dbStation.InsertOne(context.TODO(), bson.M{"sn": id, "index": 0})
+	}
+
+	// agrega el valor de la estacion como una lista
 	stationData["station"] = station
-	result, err := dbStation.UpdateOne(context.TODO(), bson.D{{Key: "sn", Value: id}, {Key: "station.station", Value: station}}, bson.D{{Key: "$set", Value: bson.D{{Key: "station", Value: stationData}}}})
-	if err != nil {
-		data.IndentedJSON(http.StatusNotImplemented, gin.H{"mensaje": "Ocurrio un error al momento de registrar la pieza"})
+
+	// checa que la estacion anteriori haya sido registrada
+	if station == stationList[0] {
+		dbStation.UpdateOne(context.TODO(), bson.M{"sn": id}, bson.D{{Key: "$set", Value: bson.D{{Key: station, Value: stationData}, {Key: "index", Value: 1}}}})
+		data.IndentedJSON(http.StatusNotImplemented, gin.H{"error": "nil", "mensaje": "Pieza Registrada con exito", "data": stationData})
 	} else {
-		data.IndentedJSON(http.StatusOK, result)
-	}
-}
-
-func addTodo(data *gin.Context) {
-	var newTodo todo
-	if err := data.BindJSON(&newTodo); err != nil {
-		return
-	}
-	todos = append(todos, newTodo)
-	data.IndentedJSON(http.StatusCreated, newTodo)
-}
-
-func getTodoById(id string) (*todo, error) {
-	for i, t := range todos {
-		if t.Id == id {
-			return &todos[i], nil
+		var stationIndex bson.M
+		dbStation.FindOne(context.TODO(), bson.M{"sn": id}).Decode(&stationIndex)
+		index := stationIndex["index"].(int32)
+		var indexInc int32
+		if index <= 4 {
+			indexInc = index + 1
+		} else {
+			indexInc = index
 		}
+		fmt.Print(index)
+		if station == stationList[index] {
+			dbStation.UpdateOne(context.TODO(), bson.M{"sn": id}, bson.D{{Key: "$set", Value: bson.D{{Key: station, Value: stationData}, {Key: "index", Value: indexInc}}}})
+			data.IndentedJSON(http.StatusNotImplemented, gin.H{"error": "nil", "mensaje": "Pieza Registrada con exito", "data": stationData})
+		} else {
+			data.IndentedJSON(http.StatusNotImplemented, gin.H{"error": "Ksr-Stn-01", "mensaje": "La pieza no ha sido registrada en las estaciones necesarias"})
+		}
+
 	}
-	return nil, errors.New("Todo Not Found")
+
 }
 
-func getTodo(data *gin.Context) {
+func getData(data *gin.Context) {
+	line := data.Param("line")
 	id := data.Param("id")
-	todo, err := getTodoById(id)
-
-	if err != nil {
-		data.IndentedJSON(http.StatusNotFound, gin.H{"message": "Todo not Found"})
-		return
-	}
-
-	data.IndentedJSON(http.StatusOK, todo)
-}
-
-func toggleTodoStatus(data *gin.Context) {
-	id := data.Param("id")
-	todo, err := getTodoById(id)
-	if err != nil {
-		data.IndentedJSON(http.StatusNotFound, gin.H{"message": "Todo not Found"})
-		return
-	}
-
-	todo.Completed = !todo.Completed
-	data.IndentedJSON(http.StatusOK, todo)
-
+	dbStation := Client.Database("kayser").Collection(line)
+	var lineData bson.M
+	dbStation.FindOne(context.TODO(), bson.M{"sn": id}).Decode(&lineData)
+	data.IndentedJSON(http.StatusCreated, lineData)
 }
--- routes.go
+++ routes.go
@@ -5,9 +5,7 @@ import "github.com/gin-gonic/gin"
 func apiRoutes(router *gin.Engine) {
 
 	router.POST("/ksr/:line/:station/:id", addPart)
-	router.GET("/monbai/:id", getTodo)
-	router.POST("/monbai", addTodo)
-	router.PATCH("/monbai/:id", toggleTodoStatus)
-	router.Run("localhost:9090")
+
+	router.GET("/ksr/getdata/:line/:id", getData)
 
 }
